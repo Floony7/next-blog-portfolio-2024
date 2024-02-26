9de79f15277e56ac3d1801781538f0169c805a20
Floony7 <fred.lunjevich@stretchsense.com>
2024-02-26 13:11:03 +0000

    Added LinkedIn to social links

--- src/components/social-links.tsx
+++ src/components/social-links.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
-import { FaYoutube, FaTwitter, FaGithub } from "react-icons/fa";
+import { FaYoutube, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function SocialLinks({ iconSize = 1 }: { iconSize?: number }) {
   const size = iconSize.toString();
 
   return (
-    <section className="flex flex-row justify-center gap-10 text-white text-4xl lg:text-5xl">
-      {/* <Link className="text-white/90 hover:text-cyan-300" href="">
-        <FaYoutube size={`${size}rem`} />
-      </Link>*/}
+    <section className="flex flex-row justify-center gap-2 text-white text-4xl lg:text-5xl">
+      <Link
+        className="text-white/85 hover:text-white"
+        href="https://www.linkedin.com/in/fred-lunjevich/"
+        target="_blank"
+      >
+        <FaLinkedin size={`${size}rem`} />
+      </Link>
       <Link
         className="text-white/85 hover:text-white"
         href="https://github.com/Floony7"
