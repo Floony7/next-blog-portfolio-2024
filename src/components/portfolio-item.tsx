d7c110eb959a12ad9dd865d9aa0defd927c69352
Floony7 <fred.lunjevich@stretchsense.com>
2024-04-20 07:58:49 +0100

    Portfolio item bg colour restored

--- src/components/portfolio-item.tsx
+++ src/components/portfolio-item.tsx
@@ -20,7 +20,7 @@ export default function PortfolioItem(props: { project: Project }) {
   } = props.project;
 
   return (
-    <section key={id} className="grid-1-2 p-4 mt-5 bg-indigo-600 rounded-lg">
+    <section key={id} className="grid-1-2 p-4 mt-5 bg-slate-800 rounded-lg">
       <div>
         <Image
           className="drop-shadow-xl shadow-black mx-auto mb-0"
