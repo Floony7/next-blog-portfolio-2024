b4bc6fa8ad38fb034a92f43f774b4504a4869346
Floony7 <fred.lunjevich@stretchsense.com>
2026-08-10 17:10:12 +0100

    Init mini league tools feature

---
+++ src/app/fpl-mini-league-tools/page.tsx
@@ -0,0 +1,10 @@
+export default async function MiniLeagueTools() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-3xl font-bold mb-4">FPL Mini League Tools</h1>
+      <p className="text-lg text-center">
+        Welcome to the FPL Mini League Tools!
+      </p>
+    </div>
+  );
+}
